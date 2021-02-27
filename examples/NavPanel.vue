9d69363e374604151703db0d9bdb66e7f3f9d61e
root <root@git.maharshi.ninja>
2021-02-27 07:33:51 +0530

    Better NavPanel implementation

--- examples/NavPanel.vue
+++ examples/NavPanel.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navbar is-link" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="db-nav-menu">
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="db-nav-menu" @click="handleBurgerBtn" :class="burgerClass">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
 
-    <div id="db-nav-menu" class="navbar-menu">
+    <div id="db-nav-menu" class="navbar-menu" :class="burgerClass">
       <div class="navbar-start">
         <router-link to="/" class="navbar-item">
           Homepage
@@ -26,7 +26,25 @@
 
 <script>
 export default {
-  name: 'NavPanel'
+  name: 'NavPanel',
+  computed: {
+    burgerClass () {
+      return {
+        'is-active': this.isBurgerOpened
+      }
+    }
+  },
+  data () {
+    return {
+      isBurgerOpened: false
+    }
+  },
+
+  methods: {
+    handleBurgerBtn () {
+      this.isBurgerOpened = !this.isBurgerOpened
+    }
+  }
 }
 </script>
 
